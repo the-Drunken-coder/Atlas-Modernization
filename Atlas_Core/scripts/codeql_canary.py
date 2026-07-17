6639c303b0686a08695fa2468030c9167375517d
the-Drunken-coder <laraujo123546@gmail.com>
2026-07-17 07:05:16 -0400

    test: remove temporary CodeQL canaries

--- Atlas_Core/scripts/codeql_canary.py
+++
@@ -1,9 +0,0 @@
-"""Intentionally vulnerable CodeQL canary. DO NOT MERGE."""
-
-import os
-
-from flask import request
-
-
-def codeql_command_injection_canary():
-    os.system(request.args["command"])
--- atlas_command_interface/src/codeql-canary.ts
+++
@@ -1,4 +0,0 @@
-// Intentionally vulnerable CodeQL canary. DO NOT MERGE.
-export function codeQLXssCanary(): void {
-  document.body.innerHTML = document.location.href;
-}
